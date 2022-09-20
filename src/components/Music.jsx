ddebba06863be560108d27b1fa598ea5963b50a4
lourdesreguera <maria.lourdes@bootcamp-upgrade.com>
2022-09-20 14:02:48 +0200

    FEAT: change spotify

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -9,13 +9,21 @@ const Music = () => {
       <div>
         <iframe
           title="player"
+          src="https://open.spotify.com/embed/track/7MwQHHzwWOC3VyLs19m6Qn?utm_source=generator"
+          className="b-music__player"
+          frameBorder="0"
+          allow="encrypted-media"
+          // loading="lazy"
+        ></iframe>
+        {/* <iframe
+          title="player"
           className="b-music__player"
           src="https://open.spotify.com/embed/track/7MwQHHzwWOC3VyLs19m6Qn?utm_source=generator&theme=0"
           frameBorder={0}
           allowFullScreen=""
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"
-        />
+        /> */}
       </div>
     </div>
   );
