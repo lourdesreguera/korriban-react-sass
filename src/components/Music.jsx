07c7cf69bf4d608e14087f79b9aec6491e8ee8b7
lourdesreguera <maria.lourdes@bootcamp-upgrade.com>
2022-09-22 16:11:37 +0200

    FEAT: remove banner mobile

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -8,14 +8,15 @@ const Music = () => {
       </div>
       <div>
         <iframe
-          title="player"
+          // width="560"
+          // height="315"
           className="b-music__player"
-          src="https://open.spotify.com/embed/track/7MwQHHzwWOC3VyLs19m6Qn?utm_source=generator&theme=0"
-          frameBorder={0}
-          allowFullScreen=""
-          allow="clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-          loading="lazy"
-        />
+          src="https://www.youtube-nocookie.com/embed/2dKBR8l65ww"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
       </div>
     </div>
   );
