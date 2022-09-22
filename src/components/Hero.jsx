0d2c8608e7942dbb6f63e2ef8cea8275bc61975e
lourdesreguera <maria.lourdes@bootcamp-upgrade.com>
2022-09-22 15:37:28 +0200

    FEAT: add banner mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Hero = () => {
-  return <div className="b-bcg b-bcg--hero" id="home"></div>;
+  return (
+    <div className="b-bcg b-bcg--hero" id="home">
+      <a href="https://open.spotify.com/album/4aCIbKKXMMG5elbUCPd4mi?si=1MlmdKS1SdWuqzrBrrjhWA" target="_blank" rel="noreferrer" className="b-hero__banner">
+        <img src="./images/banner.gif" alt="Escucha The Endless Path" />
+      </a>
+    </div>
+  );
 };
 
 export default Hero;
