97c81e20cb2edac5fa99854ad1c09cbedc3ac6c2
lourdesreguera <maria.lourdes@bootcamp-upgrade.com>
2022-09-22 15:39:47 +0200

    FEAT: remove banner mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 
 const Hero = () => {
-  return (
-    <div className="b-bcg b-bcg--hero" id="home">
-      <a href="https://open.spotify.com/album/4aCIbKKXMMG5elbUCPd4mi?si=1MlmdKS1SdWuqzrBrrjhWA" target="_blank" rel="noreferrer" className="b-hero__banner">
-        <img src="./images/banner.gif" alt="Escucha The Endless Path" />
-      </a>
-    </div>
-  );
+  return <div className="b-bcg b-bcg--hero" id="home"></div>;
 };
 
 export default Hero;
