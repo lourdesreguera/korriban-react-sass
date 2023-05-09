5bf0a3ce0f9ddb60acd059de1f1a0de90c981cc4
lourdesreguera <maria.lourdes@bootcamp-upgrade.com>
2023-05-09 15:32:59 +0200

    FEAT: add dates

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -5,23 +5,23 @@ const Tour = () => {
     <div className="b-tour" id="tour">
       <h2 className="b-tour__heading">Proximas fechas</h2>
       <div className="b-tour__dates">
-        <p>Añadiremos nuevas fechas próximamente. ¡Sigue atento!</p>
-        {/* <p>18/11/2022</p>
+        {/* <p>Añadiremos nuevas fechas próximamente. ¡Sigue atento!</p> */}
+        <p>27/05/2023</p>
         <div className="b-tour__place">
           <div className="b-tour__city">
-            <p className="b-tour__city-name">Granada</p>
-            <span>--Sala Tren</span>
+            <p className="b-tour__city-name">Huelva</p>
+            <span>--Sede Huelva Rock</span>
           </div>
-        </div> */}
+        </div>
       </div>
-      {/* <a
-        href="https://www.wegow.com/es-es/compra/bolu2-death-donuts-hole-korriban-withering-the-core-granada"
+      <a
+        href="https://entradium.com/es/events/korriban-dissidents"
         target="_blank"
         rel="noreferrer"
         className="b-btn b-btn--tour"
       >
         Comprar entradas
-      </a> */}
+      </a>
       <h2 className="b-tour__heading">Eventos pasados</h2>
       <div className="b-tour__dates">
         <p>18/11/2022</p>
